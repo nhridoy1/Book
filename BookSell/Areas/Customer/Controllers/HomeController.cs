58b2e7475427103ef8c8f1467c6d812ef0fc5d03
Ridoy <ridoyhossain5557@gmail.com>
2024-04-15 15:38:53 +0600

    Customer Home Page ready

--- BookSell/Areas/Customer/Controllers/HomeController.cs
+++ BookSell/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using BookSell.DataAccess.Data;
+using BookSell.DataAccess.Repository.IRepository;
 using BookSell.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +10,20 @@ namespace BookSell.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
+
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+
+            return View(productList);
         }
 
         public IActionResult Privacy()
