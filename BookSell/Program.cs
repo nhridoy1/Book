5756b07bbcc8c594a9bb6af86822207733c891c8
Ridoy <ridoyhossain5557@gmail.com>
2024-04-16 10:38:36 +0600

    identity scaffolidn ready and put one line in dbContext and delete areaas Applicationdb context file

--- BookSell/Program.cs
+++ BookSell/Program.cs
@@ -2,6 +2,7 @@ using BookSell.DataAccess.Data;
 using BookSell.DataAccess.Repository;
 using BookSell.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
