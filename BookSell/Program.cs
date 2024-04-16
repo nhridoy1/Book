3f750d61c2a0f53261f7581a2c7577478b3a662b
Ridoy <ridoyhossain5557@gmail.com>
2024-04-16 16:02:58 +0600

    Role got added successfully

--- BookSell/Program.cs
+++ BookSell/Program.cs
@@ -3,6 +3,8 @@ using BookSell.DataAccess.Repository;
 using BookSell.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using BookSell.Utility;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +14,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 // for login partial razor pages
 builder.Services.AddRazorPages();
 
